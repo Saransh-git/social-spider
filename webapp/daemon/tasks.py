8aea34a19dce3db940559638fecd936f49b9d8d3
Shikha Saxena <shikha@Shikhas-MacBook-Air.local>
2018-11-18 01:43:14 -0600

    data extraction using twitter streaming API

--- webapp/daemon/tasks.py
+++ webapp/daemon/tasks.py
@@ -1,6 +1,34 @@
+import json
+
+import requests
+
+from twitter.task import tweet_preprocessing
 from celery import task
-import time
+
+from utils.oauth import generate_oauth_authorization_header
+
 
 @task
 def call_twitter_stream():
-    pass
+    url = "https://stream.twitter.com/1.1/statuses/filter.json"
+    data = {'track': '@Amazon'}
+    r = requests.post(url, data=data, headers={'Authorization': generate_oauth_authorization_header(url, 'POST', data)},
+                      stream=True)
+
+    for line in r.iter_lines():
+        # filter out keep-alive new lines
+        if line:
+            decoded_line = line.decode('utf-8')
+            tweet_preprocessing.delay(json.loads(decoded_line))
+
+
+call_twitter_stream()
+
+
+# handle exceptions
+# Log exception: look how to place loggers inside a celery task/ normal python logger
+
+
+
+
+
